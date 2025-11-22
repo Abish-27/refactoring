1a46e06eb039a5bcb2ea4b732b77cc4092e34d0b
Abish Kulkarni <abishkulkarni@gmail.com>
2025-11-21 21:22:42 -0500

    Finished Task 1.2 and additional change

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -97,7 +97,7 @@ public class StatementPrinter {
     private int getVolumeCredits(Performance performance) {
         int result = 0;
         result += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
-        // add extra credit for every five comedy attendees
+        // add an extra credit for every five comedy attendees
         if ("comedy".equals(getPlay(performance).getType())) {
             result += performance.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
         }
