e96bed3d808dadb643c59ec1c93a14664ac369db
Abish Kulkarni <abishkulkarni@gmail.com>
2025-11-21 20:08:00 -0500

    Completed Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -62,8 +62,6 @@ public class StatementPrinter {
         final StringBuilder result =
                 new StringBuilder("Statement for " + getInvoice().getCustomer() + System.lineSeparator());
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (Performance performance : getInvoice().getPerformances()) {
 
             // add volume credits
@@ -73,14 +71,18 @@ public class StatementPrinter {
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
-                    frmt.format(getAmount(performance) / CENTS_IN_DOLLAR),
+                    usd(getAmount(performance)),
                     performance.getAudience()));
             totalAmount += getAmount(performance);
         }
         result.append(String.format("Amount owed is %s%n",
-                frmt.format(totalAmount / CENTS_IN_DOLLAR)));
+                usd(totalAmount)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private static String usd(int totalAmount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / CENTS_IN_DOLLAR);
     }
 
     private int getVolumeCredits(Performance performance) {
